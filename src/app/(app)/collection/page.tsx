46dd97316f9fae1e2ff9ab098e7e1dc1fa91057a
andersongama-dev <andersongamasilva5@gmail.com>
2026-04-19 19:07:12 -0300

    feat: connect collection at API

--- src/app/(app)/collection/page.tsx
+++ src/app/(app)/collection/page.tsx
@@ -2,25 +2,45 @@
 
 import CardReloj from "@/components/CardReloj";
 import FiltersBar from "@/components/Filters";
-import { FC } from "react";
+import { useState } from "react";
+import { useWatches } from "@/hooks/useWatches";
 
 export default function Collection() {
-   return (
-      <main className="pb-16">
+  const [filters, setFilters] = useState({
+    brand: [],
+    gender: [],
+    style: [],
+    caseMaterial: [],
+    strapMaterial: [],
+    strapColor: [],
+  });
 
-         <FiltersBar />
+  const { data: watches, loading } = useWatches(filters);
 
-         <div className="grid grid-cols-3 mt-32 gap-12 ml-[22dvw] mr-16">
-            <CardReloj id={1} img={"https://static.zara.net/assets/public/bdfb/1973/376e41dc8759/48ca952cc6da/T9598807308-p/T9598807308-p.jpg?ts=1775426959214&w=418"} nomeProd={"Reloj"} preco={20.00} />
-            <CardReloj id={1} img={"https://static.zara.net/assets/public/5139/a8e8/95a341edb0a2/8f81239debe1/T9533749871-p/T9533749871-p.jpg?ts=1775426891821&w=418"} nomeProd={"Reloj"} preco={20.00} />
-            <CardReloj id={1} img={"https://static.zara.net/assets/public/f1e4/ac11/a7874c5d96a0/c75cdd42ee2e/T9944633222-p/T9944633222-p.jpg?ts=1775426902148&w=418"} nomeProd={"Reloj"} preco={20.00} />
-            <CardReloj id={1} img={"https://static.zara.net/assets/public/54e1/ee20/0b3340e7baae/fd99bc3ab743/08491405802-p/08491405802-p.jpg?ts=1759240364661&w=418"} nomeProd={"Reloj"} preco={20.00} />
-            <CardReloj id={1} img={"https://static.zara.net/assets/public/eb5d/9309/b9754362a114/2eb2a9dbafe5/08491405800-p/08491405800-p.jpg?ts=1759240361441&w=418"} nomeProd={"Reloj"} preco={20.00} />
-            <CardReloj id={1} img={"https://static.zara.net/assets/public/3e49/93ed/18fb4f90ae98/4d28fcdcdc3d/05951551707-p/05951551707-p.jpg?ts=1758115878010&w=418"} nomeProd={"Reloj"} preco={20.00} />
-            <CardReloj id={1} img={"https://static.zara.net/assets/public/ebab/b10b/83c74a7d9ea3/8231f2a9961c/05862304020-p/05862304020-p.jpg?ts=1770914403766&w=418"} nomeProd={"Reloj"} preco={20.00} />
-            <CardReloj id={1} img={"https://static.zara.net/assets/public/7f4b/b4ff/052a47f98d06/0588b68660c6/08281760506-p/08281760506-p.jpg?ts=1756202745502&w=418"} nomeProd={"Reloj"} preco={20.00} />
-         </div>
-      </main>
-   );
-};
+  return (
+    <main className="pb-16">
 
+      <FiltersBar filters={filters} setFilters={setFilters} />
+
+      <div className="grid grid-cols-3 mt-32 gap-12 ml-[22dvw] mr-16">
+
+        {loading && <p>Loading...</p>}
+
+        {!loading && watches.length === 0 && (
+          <p className="text-gray-500">No watches found</p>
+        )}
+
+        {!loading &&
+          watches.map((watch: any) => (
+            <CardReloj
+              key={watch.id}
+              id={watch.id}
+              img={watch.imageUrl}
+              nomeProd={watch.name}
+              preco={Number(watch.price)}
+            />
+          ))}
+      </div>
+    </main>
+  );
+}
